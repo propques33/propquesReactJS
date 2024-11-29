6287904b3308cc4ac54c6c05cbde77d12507865c
harshrao26 <harshurao058@gmail.com>
2024-11-29 12:29:28 +0530

    thankyou page ui

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AiOutlineCheckCircle } from 'react-icons/ai'; 
 import checkgreen from '../../public/check-green.gif'
 import { Link } from "react-router-dom";
 const SuccessPage = () => {
+  useEffect(() => {
+    // Ensure scrolling to the top of the document when the component is mounted
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // You can use "auto" for instant scroll
+    });
+
+    // As a fallback, scroll the root element
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
 
   return (
     <div className="mt-20 flex items-center justify-center ">
@@ -19,7 +32,7 @@ const SuccessPage = () => {
           Thank you for submitting the form. We have received your information and will get in touch with you shortly.
         </p>
 
-        <Link to="/" className="text-blue-500">Go Back Home</Link>
+        <a href="/" className="text-blue-500">Go Back Home</a>
         
     
     
